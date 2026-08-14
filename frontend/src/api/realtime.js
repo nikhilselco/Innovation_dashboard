40177df96bbd2b8e3469474257498076e40f7298
Bhuvan Shetty <bhuvanshetty2018@gmail.com>
2026-08-14 12:05:38 +0530

    dist push to prod & some path changes

--- frontend/src/api/realtime.js
+++ frontend/src/api/realtime.js
@@ -4,7 +4,7 @@ let socket = null;
 
 function getSocket() {
   if (!socket) {
-    socket = io(import.meta.env.VITE_API_BASE_URL, {
+    socket = io("/", {
       transports: ["websocket"],
     });
   }
